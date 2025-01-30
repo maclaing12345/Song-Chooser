3207c70006b99cce28e7d02d2882dc9eef74f016
Joseph11623 <jadair28@students.stab.org>
2025-01-30 10:06:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Spotify%20Charts/Top%2050%20USA.csv"
+var position = (url,1);
+var trackName = (url,2)
+var artist = (url,3)
+var populaeity = (url,4)
+
